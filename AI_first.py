c92789d16a47d7ee606c57e3bff5125134fcf105
rhaddur <52656849+rhaddur@users.noreply.github.com>
2024-08-16 21:10:49 +0530

    youtub summary

--- AI_first.py
+++ AI_first.py
@@ -1,3 +1,7 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+video_id='2P30W3TN4nI'
 
+#video_id='aTxf5WPdwLA'
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-print("First Pandas import")
+print(transcript)
